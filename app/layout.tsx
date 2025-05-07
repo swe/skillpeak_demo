2bdfb035e7c52091de27b141ca03bac5979ddf0f
Iván A. <hello@alleksy.com>
2025-05-07 14:37:32 -0700

    hydration fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body
         className={`${inter.variable} bg-gray-50 font-inter tracking-tight text-gray-900 antialiased`}
+        style={{ isolation: "isolate" }}
       >
         <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
           {children}
